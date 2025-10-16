f621bfddeb92a18fc2a32f82f739ef487ef577bd
To_Ry <trybarczyk@edu.cdv.pl>
2025-10-16 21:00:30 +0200

    .

--- Source/ProjektUE5/ProjektUE5.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjektUE5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjektUE5, "ProjektUE5" );
-
-DEFINE_LOG_CATEGORY(LogProjektUE5)
--- Source/ProjektUE5/ProjektUE5GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjektUE5GameMode.h"
-
-AProjektUE5GameMode::AProjektUE5GameMode()
-{
-	// stub
-}
